44425686cbe8ac83d8ff0f9b3eddfbbf8363624b
lopezandresal <andresklk16@gmail.com>
2021-11-10 22:00:15 -0500

    Ejercicio 01 resuelto total.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,12 +9,53 @@
  *
  * @author andre
  */
-public class main {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        // TODO code application logic here
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingresar Tamaño Camión:");
+        int tamanioCamion = sc.nextInt();
+
+        if (tamanioCamion > 0) {
+            System.out.println("Ingresar Cantidad de Paquetes:");
+            int cantPaquetes = sc.nextInt();
+            List<Integer> paquetes = new ArrayList<>();
+
+            for (int i = 0; i < cantPaquetes; i++) {
+                System.out.println("Ingresar Paquete Número " + (i+1) + ":");
+                int paquete = sc.nextInt();
+                paquetes.add(paquete);
+            }
+
+            System.out.println("Paquetes: " + paquetes);
+
+            List<Integer> resultado = new ArrayList<>();
+
+            for (int i = 0; i < paquetes.size(); i++) {
+                int num1 = paquetes.get(i);
+
+                for (int j = 0; j < paquetes.size(); j++) {
+                    if (j != i) {
+                        int num2 = paquetes.get(j);
+
+                        if (tamanioCamion - (num1 + num2) == 30) {
+                            resultado.add(num1);
+                            resultado.add(num2);
+                            break;
+                        }
+                    }
+                }
+
+                if (resultado.size() == 2) {
+                    break;
+                }
+            }
+
+            System.out.println("\nResultado: " + resultado);
+        }
     }
 }
